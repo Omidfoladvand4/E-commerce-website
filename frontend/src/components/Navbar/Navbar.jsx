76036ae86108561708767ddc519317c5a3ad7bf4
omid <Omidfoladvand0919@gmail.com>
2025-03-22 14:06:32 +0330

    add login sign up page

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
         <li onClick={() => setMenu('kids')}><Link  style={{textDecoration : "none"}} to='/kids'>kids</Link> {menu === 'kids' ? <hr /> : <></>}</li>
        </ul>
         <div className="nav-login-cart">
-        <Link to='login'><button>Login</button></Link>
+        <Link to='/LoginSignup'><button>Login</button></Link>
          <Link to='/cart'> <img src={cart_icon} alt="" /></Link>
         <div className="nav-cart-count">0</div>
         </div>
--- frontend/src/pages/LoginSignup.jsx
+++ frontend/src/pages/LoginSignup.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import './css/LoginSignup.css'
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1> SIGN UP </h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your name' />
+          <input type="email"  placeholder='Email Address'/>
+          <input type="password"  placeholder='Your Password'/>
+
+        </div>
+        <button>Continue</button>
+        <p className='loginsignup-login'>Already have an account ? <span>Login </span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox"  name='' id=''/>
+          <p>By continuing , i agree to the terms of use & privacy policy. </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
