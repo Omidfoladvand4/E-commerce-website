d0d8c52dfb949dfd6df594a53fbaaf9f4ba3f1f2
omid <Omidfoladvand0919@gmail.com>
2025-03-22 12:10:52 +0330

    created footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Cart from './pages/Cart'
 import LoginSignup from './pages/LoginSignup'
 import Product from './pages/Product'
 import ShopCategory from './pages/ShopCategory'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path='/womens' element={<ShopCategory category='womens' />} />
         <Route path='/kids' element={<ShopCategory category='kids' />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
